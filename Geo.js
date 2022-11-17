a83d73b8f182a0ecd24b7d83fe60eccb72aa3d91
gdopera <42483260+gdopera@users.noreply.github.com>
2022-11-17 15:42:34 +0800

    Update Geo.js

--- Geo.js
+++ Geo.js
@@ -46,3 +46,6 @@ var title =flags.get(obj['countryCode']) + ' '+ City_ValidCheck(obj['city']);//+
 var subtitle = ISP_ValidCheck(obj['org']);
 var ip = obj['query'];
 var description = "国家" + ":" + obj['country'] + '\n' + "城市" + ":" + obj['city'] + '\n' + "运营商" + ":" + obj['isp'] + '\n' + "数据中心" + ":" + obj['org'];
+
+
+$done({title, subtitle, ip, description});
