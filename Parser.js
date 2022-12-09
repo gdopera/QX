f35fcbd88c6cfa3154d33a32fd72894dc4d778e6
gdopera <42483260+gdopera@users.noreply.github.com>
2022-12-09 11:15:30 +0800

    Update Parser.js

--- Parser.js
+++ Parser.js
@@ -2410,7 +2410,7 @@ function get_emoji(emojip, sname) {
   }
     str1 = JSON.stringify(Lmoji)
     aa = JSON.parse(str1)
-    bb = JSON.parse(str1.replace(/🇹🇼/g, " 🇲🇴"))
+    bb = JSON.parse(str1.replace(/🇲🇴/g, " 🇲🇴"))
     var cnt = emojip ==1? aa:bb;
     var flag = 0;
     for (var key in cnt) {
